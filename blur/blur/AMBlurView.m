8c509ca13b83df15604be530f27923f8006de40b
Cesar Pinto Castillo <cesar@blocket.se>
2013-07-28 17:29:17 +0200

    A view that blurs, done :)

--- blur/blur/AMBlurView.m
+++ blur/blur/AMBlurView.m
@@ -7,6 +7,7 @@
 //
 
 #import "AMBlurView.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface AMBlurView ()
 
@@ -17,9 +18,36 @@
 - (id)initWithCoder:(NSCoder *)aDecoder {
     self = [super initWithCoder:aDecoder];
     if (self) {
-        [self setBackgroundColor:[UIColor clearColor]];
+        [self setup];
     }
     return self;
+}
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        [self setup];
+    }
+    return self;
+}
+
+- (void)setup {
+    [self setBackgroundColor:[UIColor clearColor]];
+    [self setClipsToBounds:YES];
+    UIToolbar *toolbar = [[UIToolbar alloc] initWithFrame:[self bounds]];
+    CALayer *toolbarLayer = [toolbar layer];
+    
+    UIView *blurView = [UIView new];
+    [blurView setUserInteractionEnabled:NO];
+    [blurView.layer addSublayer:toolbarLayer];
+    [blurView setAlpha:1.0f];
+    
+    [self addSubview:blurView];
+    [blurView setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[blurView]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(blurView)]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(-1)-[blurView]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(blurView)]];
 }
 
 @end
