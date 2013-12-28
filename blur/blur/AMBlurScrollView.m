e6a26238a949b0bb0cedfd793d15fd3fde104768
Shwet Solanki <shwetsolanki@Shwets-MacBook-Pro.local>
2013-12-28 17:36:31 +0530

    blur scrollview added

--- blur/blur/AMBlurScrollView.m
+++ blur/blur/AMBlurScrollView.m
@@ -1,21 +1,21 @@
 //
-//  AMBlurView.m
+//  AMBlurScrollView.m
 //  blur
 //
-//  Created by Cesar Pinto Castillo on 7/1/13.
+//  Created by Shwet Solanki on 28/12/13.
 //  Copyright (c) 2013 Arctic Minds Inc. All rights reserved.
 //
 
-#import "AMBlurView.h"
+#import "AMBlurScrollView.h"
 #import <QuartzCore/QuartzCore.h>
 
-@interface AMBlurView ()
+@interface AMBlurScrollView ()
 
 @property (nonatomic, strong) UIToolbar *toolbar;
 
 @end
 
-@implementation AMBlurView
+@implementation AMBlurScrollView
 
 - (instancetype)initWithCoder:(NSCoder *)aDecoder {
     self = [super initWithCoder:aDecoder];
@@ -46,7 +46,6 @@
     // If we don't clip to bounds the toolbar draws a thin shadow on top
     [self setClipsToBounds:YES];
     [self setBackgroundColor:[UIColor clearColor]];
-    
     if (![self toolbar]) {
         [self setToolbar:[[UIToolbar alloc] initWithFrame:[self bounds]]];
         [self.layer insertSublayer:[self.toolbar layer] atIndex:0];
