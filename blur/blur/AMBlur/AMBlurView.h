9526c1638c60d6138c5ffcebeecc7d4808749f81
Shwet Solanki <shwetsolanki@Shwets-MacBook-Pro.local>
2013-12-28 17:41:59 +0530

    added AMBlurScrollView

---
+++ blur/blur/AMBlur/AMBlurView.h
@@ -0,0 +1,16 @@
+//
+//  AMBlurView.h
+//  blur
+//
+//  Created by Cesar Pinto Castillo on 7/1/13.
+//  Copyright (c) 2013 Arctic Minds Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AMBlurView : UIView
+
+// Use the following property to set the tintColor. Set it to nil to reset.
+@property (nonatomic, strong) UIColor *blurTintColor;
+
+@end
---
+++ blur/blur/AMBlur/AMBlurView.m
@@ -0,0 +1,65 @@
+//
+//  AMBlurView.m
+//  blur
+//
+//  Created by Cesar Pinto Castillo on 7/1/13.
+//  Copyright (c) 2013 Arctic Minds Inc. All rights reserved.
+//
+
+#import "AMBlurView.h"
+#import <QuartzCore/QuartzCore.h>
+
+@interface AMBlurView ()
+
+@property (nonatomic, strong) UIToolbar *toolbar;
+
+@end
+
+@implementation AMBlurView
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        [self setup];
+    }
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self setup];
+    }
+    return self;
+}
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        [self setup];
+    }
+    return self;
+}
+
+- (void)setup {
+    // If we don't clip to bounds the toolbar draws a thin shadow on top
+    [self setClipsToBounds:YES];
+    [self setBackgroundColor:[UIColor clearColor]];
+    
+    if (![self toolbar]) {
+        [self setToolbar:[[UIToolbar alloc] initWithFrame:[self bounds]]];
+        [self.layer insertSublayer:[self.toolbar layer] atIndex:0];
+    }
+}
+
+- (void) setBlurTintColor:(UIColor *)blurTintColor {
+    [self.toolbar setBarTintColor:blurTintColor];
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    [self.toolbar setFrame:[self bounds]];
+}
+
+@end
